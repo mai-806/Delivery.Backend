72e04c2b329710224966c5734cb044be96e1402f
timur <vindorwolf@gmail.com>
2023-07-05 16:08:10 +0300

    fix SQLquery

--- deli-main/src/models/sql/select_orders.hpp
+++ deli-main/src/models/sql/select_orders.hpp
@@ -3,7 +3,7 @@
 
 namespace deli_main::models::sql {
   const constexpr char *kSelectOrders = "SELECT * "
-                                       "FROM deli_main.couriers "
+                                       "FROM deli_main.orders "
                                        "WHERE courier = $1.id "
                                        "RETURNING id;";
 } // namespace deli_main::models::sql
