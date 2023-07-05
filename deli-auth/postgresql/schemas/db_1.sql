0373bdad4d19befccd393fe97ed82d5fcaf68c71
Eugene Tikhonov <jean2003jean@mail.ru>
2023-07-06 00:41:42 +0300

    Adding new types and tables

--- deli-auth/postgresql/schemas/db_1.sql
+++ deli-auth/postgresql/schemas/db_1.sql
@@ -1,8 +1,25 @@
-DROP SCHEMA IF EXISTS hello_schema CASCADE;
+DROP SCHEMA IF EXISTS deli_auth CASCADE;
 
-CREATE SCHEMA IF NOT EXISTS hello_schema;
+CREATE SCHEMA IF NOT EXISTS deli_auth;
 
-CREATE TABLE IF NOT EXISTS hello_schema.users (
-    name TEXT PRIMARY KEY,
-    count INTEGER DEFAULT(1)
+CREATE TYPE deli_auth.user_type AS ENUM (
+    'customer',
+    'courier',
+    'admin'
+);
+
+CREATE TABLE IF NOT EXISTS deli_auth.users (
+    id          BIGSERIAL PRIMARY KEY,
+    login       text NOT NULL,
+    password    text NOT NULL,
+    user_type   deli_auth.user_type DEFAULT 'customer' NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS ix_deli_auth_users_login ON deli_auth.users (login);
+CREATE INDEX IF NOT EXISTS ix_deli_auth_users_user_type ON deli_auth.users (user_type);
+
+CREATE TYPE deli_auth.v1_user AS (
+    id          BIGINT,
+    login       text,
+    user_type   deli_auth.user_type
 );
