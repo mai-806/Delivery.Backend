9e6ceb26303f44632bac784e9f76c0775bf307f5
timur <vindorwolf@gmail.com>
2023-07-07 15:13:13 +0300

    models

--- deli-auth/src/models/requests.hpp
+++ deli-auth/src/models/requests.hpp
@@ -6,4 +6,7 @@
 
 namespace deli_auth::models::requests {
 
+  int64_t UpdateUserPassword(const userver::storages::postgres::ClusterPtr& cluster,
+                      const models::User &user);
+
 } // namespace deli_auth::models::requests
