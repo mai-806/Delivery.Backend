8bb592dcb8f9434d7c6c8e30db13c4dc4706d83b
timur <vindorwolf@gmail.com>
2023-07-07 15:12:17 +0300

    models

--- deli-auth/src/models/requests.cpp
+++ deli-auth/src/models/requests.cpp
@@ -4,4 +4,12 @@
 
 namespace deli_auth::models::requests {
 
+  int64_t UpdateUserPassword(const userver::storages::postgres::ClusterPtr& cluster,
+                      const models::User &user) {
+    const auto &result =
+            cluster->Execute(userver::storages::postgres::ClusterHostType::kMaster,
+                             sql::kUpdateUserPassword,
+                             user);
+    return result.AsSingleRow<int64_t>();
+  }
 } // namespace deli_auth::models::requests
--- deli-auth/src/models/sql/update_user_password.hpp
+++ deli-auth/src/models/sql/update_user_password.hpp
@@ -3,6 +3,6 @@
 
 namespace deli_auth::models::sql {
   const constexpr char *kUpdateUserPassword = "UPDATE deli_auth.users "
-                                              "SET password = $2 "
-                                              "WHERE id = $1;";
+                                              "SET password = $1.password "
+                                              "WHERE id = $1.id;";
 } // namespace deli_auth::models::sql
