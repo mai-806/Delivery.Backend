417a8e02ffa974be65e8bd05b9ed76c616c51348
Eugene Tikhonov <jean2003jean@mail.ru>
2023-07-07 12:38:53 +0300

    Added my endpoint

--- deli-auth/src/main.cpp
+++ deli-auth/src/main.cpp
@@ -7,7 +7,7 @@
 
 #include <common/consts.hpp>
 #include <components/requester.hpp>
-#include <views/v1/order/post/handler.hpp>
+#include <views/v1/user/patch/handler.hpp>
 
 int main(int argc, char *argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]) {
           .Append<userver::components::TestsuiteSupport>()
           .Append<userver::components::HttpClient>()
           .Append<userver::server::handlers::TestsControl>()
-          .Append<deli_main::components::Requester>()
-          .Append<deli_main::views::v1::order::post::Handler>()
+          .Append<deli_auth::components::Requester>()
+          .Append<deli_auth::views::v1::user::patch::Handler>()
           .Append<userver::clients::dns::Component>()
-          .Append<userver::components::Postgres>(deli_main::common::consts::kPgClusterName);
+          .Append<userver::components::Postgres>(deli_auth::common::consts::kPgClusterName);
 
 
   return userver::utils::DaemonMain(argc, argv, component_list);
