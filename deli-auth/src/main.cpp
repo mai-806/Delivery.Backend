a9923303446ca5722d73073186a06f099f608731
timur <vindorwolf@gmail.com>
2023-07-07 17:31:49 +0300

    main

--- deli-auth/src/main.cpp
+++ deli-auth/src/main.cpp
@@ -5,12 +5,17 @@
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
+#include <common/consts.hpp>
+#include <components/requester.hpp>
+#include <views/v1/auth/user/reset/post/handler.hpp>
 
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
+                            .Append<deli_auth::components::Requester>()
+                            .Append<deli_auth::views::v1::auth::user::reset::post::Handler>()
                             .Append<userver::server::handlers::TestsControl>();
 
   deli_auth::AppendHello(component_list);
