bca9ee09588ee41b19335d7296bfd764acf7767b
timur <vindorwolf@gmail.com>
2023-07-07 12:28:50 +0300

    db init

--- deli-auth/postgresql/schemas/db_1.sql
+++ deli-auth/postgresql/schemas/db_1.sql
@@ -1,8 +1,19 @@
-DROP SCHEMA IF EXISTS hello_schema CASCADE;
+DROP SCHEMA IF EXISTS deli_auth CASCADE;
 
-CREATE SCHEMA IF NOT EXISTS hello_schema;
+CREATE SCHEMA IF NOT EXISTS deli_auth;
 
-CREATE TABLE IF NOT EXISTS hello_schema.users (
-    name TEXT PRIMARY KEY,
-    count INTEGER DEFAULT(1)
+CREATE TYPE deli_auth.user_type AS ENUM (
+    'customer',
+    'courier',
+    'admin'
+    );
+
+CREATE TABLE IF NOT EXISTS deli_auth.users
+(
+    id          BIGSERIAL PRIMARY KEY,
+    login       text NOT NULL,
+    password    text NOT NULL,
+    user_type   deli_auth.user_type DEFAULT 'customer' NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS ix_deli_auth_users_login ON deli_auth.users (login);
