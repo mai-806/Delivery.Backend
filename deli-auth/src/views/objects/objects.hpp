bfe95488501372453d2b7a9aefeaa9d968938671
Eugene Tikhonov <jean2003jean@mail.ru>
2023-07-08 15:45:48 +0300

    Removed registration object

--- deli-auth/src/views/objects/objects.hpp
+++ deli-auth/src/views/objects/objects.hpp
@@ -9,18 +9,6 @@ namespace deli_auth::views {
     std::string message;
   };
 
-  namespace v1::auth::register {
-    struct RegisterRequest {
-      std::string login;
-      std::string password;
-
-    };
-
-    struct RegisterResponse {
-      int64_t order_id;
-    };
-  };
-
   namespace v1::user::patch {
     struct UserUpdateRequest {
       int64_t id;
