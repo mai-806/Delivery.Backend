b85cbc7adcaef3e0968b44ea1a09312fe06f3045
timur <vindorwolf@gmail.com>
2023-07-10 00:43:31 +0300

    FIX

--- deli-auth/src/views/objects/parsers.cpp
+++ deli-auth/src/views/objects/parsers.cpp
@@ -111,39 +111,39 @@ namespace {
 
 namespace userver::formats::parse {
 
-  RegisterRequest Parse(const userver::formats::json::Value &elem,
-        userver::formats::parse::To<RegisterRequest>) {
-    const Keys required_keys = {"login", "user_type"};
-    const Keys optional_keys;
-    const Types key_types = {
-            {"login",         FieldType::kString},
-            {"user_type",     FieldType::kString}
-    };
+    RegisterRequest Parse(const userver::formats::json::Value &elem,
+                          userver::formats::parse::To<RegisterRequest>) {
+        const Keys required_keys = {"login", "user_type"};
+        const Keys optional_keys;
+        const Types key_types = {
+                {"login",     FieldType::kString},
+                {"user_type", FieldType::kString}
+        };
 
-    CheckFields(required_keys, optional_keys, key_types, elem);
-    LOG_DEBUG() << "fields are checked";
-    std::string user_type = GetRequiredValue<std::string>(elem, "user_type");
-    UserType enum_user_type;
-    if(user_type == "customer"){
-      enum_user_type = UserType::kUserTypeCustomer;
-    } else if (user_type == "courier"){
-      enum_user_type = UserType::kUserTypeCourier;
-    } else if (user_type == "admin"){
-      enum_user_type = UserType::kUserTypeAdmin;
-    } else {
-      throw userver::formats::json::ParseException(
-                fmt::format("user_type is shit"));
+        CheckFields(required_keys, optional_keys, key_types, elem);
+        LOG_DEBUG() << "fields are checked";
+        std::string user_type = GetRequiredValue<std::string>(elem, "user_type");
+        UserType enum_user_type;
+        if (user_type == "customer") {
+            enum_user_type = UserType::kUserTypeCustomer;
+        } else if (user_type == "courier") {
+            enum_user_type = UserType::kUserTypeCourier;
+        } else if (user_type == "admin") {
+            enum_user_type = UserType::kUserTypeAdmin;
+        } else {
+            throw userver::formats::json::ParseException(
+                    fmt::format("user_type is shit"));
+        }
+        // required fields:
+        RegisterRequest register_request{
+                .login = GetRequiredValue<std::string>(elem, "login"),
+                .user_type = enum_user_type
+        };
+        LOG_DEBUG() << "request parsed";
+        return register_request;
     }
-    // required fields:
-    RegisterRequest register_request{
-            .login = GetRequiredValue<std::string>(elem, "login"),
-            .user_type = enum_user_type
-    };
-    LOG_DEBUG() << "request parsed";
-    return register_request;
-  }
-  
-  UserResetRequest
+
+    UserResetRequest
     Parse(const userver::formats::json::Value &elem,
           userver::formats::parse::To<UserResetRequest>) {
         const Keys required_keys = {"id"};
@@ -160,27 +160,26 @@ namespace userver::formats::parse {
         };
         LOG_DEBUG() << "request parsed";
         return user_reset_request;
-
+    }
 } // namespace userver::formats::parse
-
 
 namespace userver::formats::serialize {
 
-  json::Value Serialize(const deli_auth::views::ErrorResponse &value,
-                        serialize::To<json::Value>) {
-    json::ValueBuilder builder;
+    json::Value Serialize(const deli_auth::views::ErrorResponse &value,
+                          serialize::To<json::Value>) {
+        json::ValueBuilder builder;
 
-    builder["message"] = value.message;
+        builder["message"] = value.message;
 
-    return builder.ExtractValue();
-  }
+        return builder.ExtractValue();
+    }
 
-  json::Value Serialize(const RegisterResponse &value,
-                        serialize::To<json::Value>) {
-    json::ValueBuilder builder;
+    json::Value Serialize(const RegisterResponse &value,
+                          serialize::To<json::Value>) {
+        json::ValueBuilder builder;
 
-    builder["id"] = value.id;
+        builder["id"] = value.id;
 
-    return builder.ExtractValue();
-  }
+        return builder.ExtractValue();
+    }
 } // namespace userver::formats::serialize
