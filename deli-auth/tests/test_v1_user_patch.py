f505e6eda16ced9ddf49974f3beec0f825598909
Eugene Tikhonov <jean2003jean@mail.ru>
2023-07-10 18:36:59 +0300

    Added and corrected tests

--- deli-auth/tests/test_v1_user_patch.py
+++ deli-auth/tests/test_v1_user_patch.py
@@ -1,33 +1,82 @@
 import pytest
+from testsuite.databases import pgsql
 
-@pytest.mark.parametrize(
-    'id, login, type, expected_response',
-    [
-        (1, 'new_login', 'customer', {'id': 1, 'login': 'new_login', 'type': 'customer'}),
-        (2, None, 'courier', {'id': 2, 'login': 'old_login', 'type': 'courier'}),
-        (3, 'new_login', None, {'id': 3, 'login': 'new_login', 'type': 'admin'}),
-        (4, None, None, {'message': 'Wrong request: at least one parameter must be provided'}),
-        (5, 'new_login', 'wrong_type', {'message': 'Wrong request: parameter \'type\' must be one of [customer/courier/admin]'}),
-        (None, 'new_login', 'customer', {'message': 'Wrong request: parameter \'id\' is required'}),
-        (6, None, None, {'message': 'User not found'})
-    ]
+
+@pytest.mark.pgsql(
+    'db_1',
+    queries=[
+        """INSERT INTO deli_auth.users
+        (login, password, user_type)
+        VALUES
+        ('jeantik', '1234', 'admin')
+        """,
+        ],
 )
-async def test_v1_user_patch(self, service_client, id, login, type,
-                             expected_response):
-    data = {}
-    if id:
-        data['id'] = id
-    if login:
-        data['login'] = login
-    if type:
-        data['type'] = type
+@pytest.mark.parametrize(
+    'request_body, expected_response_body, expected_response_code',
+    [
+        pytest.param(
+            {
+                "id": 1,
+                "login": "jeantik228",
+                "user_type": "customer"
+            },
+            {
+                'id': 1,
+                'login': 'jeantik228',
+                'user_type': 'customer'
+            },
+            200,
+            id='OK',
+        ),
+        pytest.param(
+            {
+                "id": 1,
+                "user_type": "customer"
+            },
+            {
+                'id': 1,
+                'login': 'jeantik',
+                'user_type': 'customer'
+            },
+            200,
+            id='OK',
+        ),
+        pytest.param(
+            {
+                "id": 1,
+                "use_type": "customer"
+            },
+            {"message": "user_type is invalid"},
+            400,
+            id='error in request - wrong key name',
+        ),
+        pytest.param(
+            {
+                "id": 1,
+                "user_type": "lol"
+            },
+            {"message": "user_type is invalid"},
+            400,
+            id='error in request - wrong user_type',
+        ),
+        pytest.param(
+            {
+                "id": 9999999,
+                "login": "new_login",
+                "user_type": "customer"
+            },
+            {"message": "user not found"},
+            404,
+            id='error - user not found',
+        ),
+    ],
+)
 
-    response = await service_client.patch(
-        '/v1/user',
-        json=data
-    )
-    if expected_response.get('message'):
-        assert response.status == 400 or response.status == 404
-    else:
-        assert response.status == 200
-    assert response.json() == expected_response
+async def test_patch_user(service_client, request_body, expected_response_body, expected_response_code):
+        user_id = request_body["id"]
+        response = await service_client.patch("/v1/user", json=request_body)
+        assert response.status == expected_response_code
+        response_data = response.json()
+        assert response_data == expected_response_body
+        
