53b83b2c7739a0e3d6004d5654aaeb2854e248be
RoKivals <slava111103@gmail.com>
2023-07-11 03:25:53 +0300

    Tests completed

--- deli-main/tests/test_v1_order_get.py
+++ deli-main/tests/test_v1_order_get.py
@@ -1,5 +1,10 @@
 import pytest
 
+data_sql = f"""
+((22, 11), (23, 91), 'in_progress', 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+"""
+
+
 @pytest.mark.parametrize(
     'request_body, expected_response_body, '
     'expected_response_code',
@@ -23,11 +28,42 @@ import pytest
             200,
             id='OK',
         ),
+        pytest.param(
+            {
+                'customerId': 1,
+            },
+            {'message': 'Key \'order_id\' is missing but required'},
+            400,
+            id='error in request',
+        ),
+        pytest.param(
+            {
+                'order_id': -3,
+            },
+            {'message': 'id parameter should be above 0'},
+            400,
+            id='id parameter should be above 0',
+        ),
+        pytest.param(
+            {
+                'order_id': 4,
+            },
+            {'message': 'order not found'},
+            404,
+            id='order not found',
+        ),
     ],
 )
-async def test_v1_order_get(service_client, request_body,
-                            expected_response_body,
-                            expected_response_code):
+async def test_v1_order_post(service_client, request_body,
+                             expected_response_body,
+                             expected_response_code, pgsql):
+    cursor = pgsql['db_1'].cursor()
+    cursor.execute(
+        f"""INSERT INTO deli_main.orders 
+           VALUES {data_sql}
+        """
+)
+
     response = await service_client.get(
         '/v1/order',
         json=request_body,
