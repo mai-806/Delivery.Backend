02855a05e46eefbf9300a1e861e7eb11963f562d
Eugene Tikhonov <jean2003jean@mail.ru>
2023-07-11 16:55:53 +0300

    Fixed bages 2

--- deli-auth/src/models/sql/all.hpp
+++ deli-auth/src/models/sql/all.hpp
@@ -12,6 +12,5 @@
 #include <models/sql/insert_user.hpp>
 #include <models/sql/get_user.hpp>
 #include <models/sql/check_user_exist.hpp>
-#include <models/sql/get_user_by_id.hpp>
 #include <models/sql/update_user_login.hpp>
 #include <models/sql/update_user_type.hpp>
